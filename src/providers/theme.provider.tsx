7ce39f25625b6ebde2f7f6e270ecccee428cb6f6
freitassdev <chelmizinho@gmail.com>
2024-12-29 17:53:08 -0300

    feat: setup docker

--- src/providers/theme.provider.tsx
+++ src/providers/theme.provider.tsx
@@ -3,17 +3,17 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes";
-import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = useState(false);
+  const [isMounted, setIsMounted] = React.useState<boolean>(false);
 
-  useEffect(() => {
-    setMounted(true);
+  React.useEffect(() => {
+    setIsMounted(true);
   }, []);
 
-  if (!mounted) {
-    return <>{children}</>; // Render children without ThemeProvider during SSR
+  if (!isMounted) {
+    return null;
   }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
