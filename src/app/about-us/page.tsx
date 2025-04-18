903c668583d59f25bd6a49d39cd8390a012ac9a0
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-04-18 16:50:56 -0300

    feat: add backgrounds

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,12 +1,33 @@
-import AboutUsPage from "@/components/global/about-us";
-import Footer from "@/components/global/footer";
+import { CardsCarousel } from "@/components/global/cards-carrousel";
+import { AnimatedTooltipPreview } from "@/components/global/team-tooltip";
 import Navbar from "@/components/shared/navbar";
+import Footer from "@/components/global/footer";
 
 export default function AboutUs() {
   return (
     <div className="w-full relative">
       <Navbar />
-      <AboutUsPage />
+      <div className="w-screen h-full relative bg-background">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="absolute h-screen w-screen bg-background -z-0">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f10_1px,transparent_1px),linear-gradient(to_bottom,#80808007_1px,transparent_1px)] bg-[size:14px_24px]" />
+            <div className="absolute right-1/2 top-[8%] h-[800px] w-[800px] rounded-full bg-[radial-gradient(circle_300px_at_50%_300px,rgba(77,139,255,.15),#171821a1)]" />
+            <div className="w-screen h-10 absolute bottom-0 bg-background/70 blur-sm" />
+          </div>
+          <h1 className="text-6xl font-bold z-20">História</h1>
+          <p className="w-1/3 text-lg text-justify z-20">
+            A Flyra nasceu com o objetivo de integrar tecnologia avançada para
+            solucionar problemas ambientais em áreas verdes e urbanas. Nosso
+            projeto surgiu da preocupação com a degradação ambiental e a
+            necessidade de respostas rápidas e eficientes no combate a incêndios
+            florestais. Através do uso de drones equipados com inteligência
+            artificial, buscamos criar um sistema inovador para monitoramento e
+            prevenção de desastres.
+          </p>
+        </div>
+        <CardsCarousel />
+        <AnimatedTooltipPreview />
+      </div>
       <Footer />
     </div>
   );
--- src/components/global/team-tooltip.tsx
+++ src/components/global/team-tooltip.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { AnimatedTooltip } from "../ui/animated-tooltip";
 import { ChevronRight } from "lucide-react";
 
@@ -10,33 +11,33 @@ const people = [
   },
   {
     id: 2,
-    name: "Matheus Pascheto",
-    designation: "Sub-gerente/Desenvolvedor",
-    image: "/images/team/matheus.jpeg",
+    name: "Gustavo Oliveira",
+    designation: "Marketing/Designer",
+    image: "/images/team/gustavo.jpeg",
   },
   {
     id: 3,
-    name: "Michel Freitas",
-    designation: "Gerente/Desenvolvedor",
-    image: "/images/team/michel.jpg",
-  },
-  {
-    id: 4,
     name: "Leonardo Elias",
     designation: "Pesquisador de Campo/Documentação",
     image: "/images/team/leonardo.jpeg",
   },
   {
-    id: 5,
+    id: 4,
     name: "Lucas Goulart",
     designation: "Engenheiro/Pesquisador de Campo",
     image: "/images/team/lucas.jpeg",
   },
   {
+    id: 5,
+    name: "Matheus Pascheto",
+    designation: "Sub-gerente/Desenvolvedor",
+    image: "/images/team/matheus.jpeg",
+  },
+  {
     id: 6,
-    name: "Ryan Santos",
-    designation: "Designer/Documentação",
-    image: "/images/team/ryan.jpeg",
+    name: "Michel Freitas",
+    designation: "Gerente/Desenvolvedor",
+    image: "/images/team/michel.jpg",
   },
   {
     id: 7,
@@ -52,9 +53,9 @@ const people = [
   },
   {
     id: 9,
-    name: "Gustavo Oliveira",
-    designation: "Marketing/Designer",
-    image: "/images/team/gustavo.jpeg",
+    name: "Ryan Santos",
+    designation: "Designer/Documentação",
+    image: "/images/team/ryan.jpeg",
   },
   {
     id: 10,
@@ -70,13 +71,19 @@ export function AnimatedTooltipPreview() {
       id="animatedTeamComponent"
       className="h-[50vh] flex flex-col items-center justify-center gap-5 py-72"
     >
+      <div className="relative h-full w-full bg-slate-950">
+        <div className="absolute bottom-0 left-[-20%] right-0 top-[20%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(77,139,255,.15),rgba(255,255,255,0))]" />
+        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(77,139,255,.15),rgba(255,255,255,0))]" />
+      </div>
       <h1 className="text-6xl font-bold z-20">Nosso Time</h1>
       <div className="flex flex-row items-center justify-center w-full">
         <AnimatedTooltip items={people} />
       </div>
-      <button className="py-4 flex justify-center items-center mt-6 gap-2 rounded text-zinc-50 font-semibold pl-4 pr-2 hover:bg-zinc-50/30 transition-all hover:translate-x-1 z-20">
-        <p>Contate-nos</p> <ChevronRight />
-      </button>
+      <Link href="/contact">
+        <button className="py-4 flex justify-center items-center mt-6 gap-2 rounded text-foreground font-semibold pl-4 pr-2 hover:bg-zinc-50/30 transition-all hover:translate-x-1 z-20">
+          <p>Contate-nos</p> <ChevronRight />
+        </button>
+      </Link>
     </div>
   );
 }
