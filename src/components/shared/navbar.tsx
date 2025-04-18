dd8b5570f8328b4aeb31843f25285488813178da
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-04-18 16:53:13 -0300

    feat: add links

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,43 +1,14 @@
 "use client";
 import Image from "next/image";
 import logo from "@/assets/logos/logo-full-373x131.png";
-// import Link from "next/link";
-// import { LucideDot } from "lucide-react";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import { HoveredLink, Menu, MenuItem } from "../ui/navbar-menu";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-interface INavbarItem {
-  name: string;
-  link: string;
-}
-
-const navbarItems: INavbarItem[] = [
-  {
-    name: "Início",
-    link: "#",
-  },
-  {
-    name: "Perguntas Frequentes",
-    link: "#",
-  },
-  {
-    name: "Sobre nós",
-    link: "#",
-  },
-];
-
 export default function Navbar() {
-  const [isMobileOpened, setIsMobileOpened] = useState<boolean>(true);
   const [active, setActive] = useState<string | null>(null);
-
-  const router = useRouter();
-  const redirectToLoginPage = () => {
-    router.push("/auth/login");
-  };
 
   return (
     <div className={cn("fixed top-5 inset-x-0 max-w-6xl mx-auto z-50")}>
@@ -52,26 +23,40 @@ export default function Navbar() {
                 item="Institucional"
               >
                 <div className="flex flex-col space-y-4 text-sm">
-                  <HoveredLink href="/about">Sobre nós</HoveredLink>
+                  <HoveredLink href="/about-us">Sobre nós</HoveredLink>
                   <HoveredLink href="/team">Nossa Equipe</HoveredLink>
                   <HoveredLink href="/faq">
                     Perguntas Frequentes (FAQ)
                   </HoveredLink>
-                  <HoveredLink href="/branding">Contato</HoveredLink>
+                  <HoveredLink href="/contact">Contato</HoveredLink>
                 </div>
               </MenuItem>
               <MenuItem setActive={setActive} active={active} item="Nossa Rede">
                 <div className="flex flex-col space-y-4 text-sm">
-                  <HoveredLink href="/hobby">Linkedin</HoveredLink>
-                  <HoveredLink href="/individual">Github</HoveredLink>
-                  <HoveredLink href="/team">Instagram</HoveredLink>
+                  <Link
+                    target="_blank"
+                    href="https://www.linkedin.com/company/flyra"
+                  >
+                    Linkedin
+                  </Link>
+                  <Link target="_blank" href="https://github.com/Flyra-Drones">
+                    Github
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://www.instagram.com/flyra.tech"
+                  >
+                    Instagram
+                  </Link>
                 </div>
               </MenuItem>
               <Link href="/blog/post/flyra-drones-inteligentes-para-combate-a-incendios-em-areas-remotas">
                 <p className="text-md">Blog</p>
               </Link>
             </div>
-            <Button className="px-6 rounded-sm">Download</Button>
+            <Link href="/download">
+              <Button className="px-6 rounded-sm">Download</Button>
+            </Link>
           </div>
         </div>
       </Menu>
